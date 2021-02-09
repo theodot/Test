a290db285b0f8e4bdc08077b984f35945ac97fd1
Tony Theodoropoulos <theodot@outlook.com>
2021-02-09 10:02:10 -0500

    testing changes

--- Test/Form1.Designer.cs
+++ Test/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace Test
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(430, 379);
+            this.ClientSize = new System.Drawing.Size(352, 379);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
